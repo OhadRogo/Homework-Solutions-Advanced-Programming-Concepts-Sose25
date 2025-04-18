7aa74adddb609b97f17795b1c62f8cf78e4b562c
OhadRogo <ohad772@gmail.com>
2025-04-18 15:25:06 +0200

    test eclips commit

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,6 +1,12 @@
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("hi");
+        System.out.println("Hello");
+        System.out.println("Holla");
+        
+        
+
+        
     }
 }
 
